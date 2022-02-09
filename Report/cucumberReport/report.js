c3ba4dc863cdf6757bdbe91006529e303fec6a95
purvainfostride <97033527+purvainfostride@users.noreply.github.com>
2022-02-09 18:33:12 +0530

    containing Reports

---
+++ Report/cucumberReport/report.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("C:/Users/Purva/eclipse-workspace/bddFramework.HRM/src/test/java/featureFiles/Login.feature");
+formatter.feature({
+  "name": "Checking Login functionality of the application",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "name": "Checking Login functionality with the valid login credentials",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "user is on Login page",
+  "keyword": "Given "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "user enters username in the username textfield and password in password textfield",
+  "keyword": "When "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "user clicks on the Login button",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "user is on Homepage",
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+});
