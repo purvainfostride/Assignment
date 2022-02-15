58e6be5fb3b251a405b718f3e38152e8f33673bb
purvainfostride <97033527+purvainfostride@users.noreply.github.com>
2022-02-16 04:39:49 +0530

    Update SignupPage.java

--- pages/SignupPage.java
+++ pages/SignupPage.java
@@ -1,12 +1,10 @@
 package pages;
 
 import java.time.Duration;
-
 import org.apache.commons.lang.RandomStringUtils;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
 import utility.Base;
 
 public class SignupPage extends Base{
